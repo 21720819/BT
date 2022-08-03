4906be71442e700428e44dbbb26ac7edeb292750
minsol1 <81753858+minsol1@users.noreply.github.com>
2022-08-03 20:35:39 +0900

    자유게시판 수정삭제

--- free/urls.py
+++ free/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('create/', views.freeCreate, name='freeCreate'),
     path('detail/<int:free_id>', views.freeDetail, name='freeDetail'),
     path('create_comment/<int:free_id>',views.create_comment, name='create_comment'),
+    path('detail/<int:free_id>/delete',views.freeDelete,name='freeDelete'),
+    path('detail/<int:free_id>/edit',views.freeEdit,name='freeEdit'),
 ]
--- free/views.py
+++ free/views.py
@@ -37,3 +37,32 @@ def create_comment(request, free_id):
         finished_form.save()
 
     return redirect('freeDetail',free_id)
+
+def freeDelete(request, free_id):
+    post = Free.objects.get(pk=free_id)
+    post.delete()
+    return redirect('freeHome')
+
+def freeEdit(request, free_id):
+    post = Free.objects.get(id=free_id)
+    # 글을 수정사항을 입력하고 제출을 눌렀을 때
+    if request.method == "POST":
+        form = Freemodelform(request.POST, request.FILES)
+        if form.is_valid():
+            print(form.cleaned_data)
+            # {'name': '수정된 이름', 'image': <InMemoryUploadedFile: Birman_43.jpg 	(image/jpeg)>, 'gender': 'female', 'body': '수정된 내용'}
+            post.title = form.cleaned_data['name']
+            post.photo = form.cleaned_data['image']
+            post.body = form.cleaned_data['body']
+            post.save()
+            return redirect('/detail/'+str(post.pk))
+        
+    # 수정사항을 입력하기 위해 페이지에 처음 접속했을 때
+    else:
+        form = Freemodelform(instance =post )
+        context={
+            'form':form,
+            'writing':True,
+            'now':'edit',
+        }
+        return render(request, 'free/edit_post.html',context)
