c7e06d27f648b00166014e8339ea291b50ab02ad
JuHyun Yoon <21720819@gmail.com>
2022-08-27 14:38:27 +0900

    거래_작성_조회

--- buy/forms.py
+++ buy/forms.py
@@ -7,25 +7,45 @@ class BuyModelform(forms.ModelForm):
 		fields = ['title','body','date','category','wpeople','price','location','photo']
 
 		widgets = {
-			'title' : forms.TextInput(
-				attrs={'class': 'form-control', 'style': 'width: 400px', 'placeholder': '제목을 입력하세요.', 'name' : 'title', 'id' : 'title'}
-			),
-			'body' : forms.Textarea (
-				attrs={'class': 'form-control', 'cols':'50', 'rows':'15', 'style': 'width: 400px', 'placeholder': '내용을 입력하세요.'}
-			),
-			'date' : forms.TextInput(
-				attrs={'class': 'form-control', 'style': 'width: 200px', 'placeholder': '날짜를 입력하세요.', 'name' : 'date', 'id' : 'date'}
-			),
-			'price' : forms.TextInput(
-				attrs={'class': 'form-control', 'style': 'width: 150px', 'name' : 'price', 'id' : 'price'}
-			),
-			'wpeople' : forms.TextInput(
-				attrs={'class': 'form-control', 'style': 'width: 100px', 'name' : 'wpeople', 'id' : 'wpeople'}
-			),
-			'location' : forms.TextInput(
-				attrs={'class': 'form-control', 'style': 'width: 200px', 'placeholder': '장소를 입력하세요. ', 'name' : 'place', 'id' : 'place'}
+			'photo' : forms.ClearableFileInput(
+				attrs={ 'style': 'visibility: hidden; position: absolute; z-index: 1;',
+           				'onchange': 'imageView(this)',
+						'name' : 'photo', 'id' : 'photo' }
 			),
 			'category' : forms.Select(
-				attrs={'class': 'custom-select'}
+				attrs={ 'class': 'custom-select', 'style': 'border: 0;' }
+			),
+			'title' : forms.TextInput(
+				attrs={ 'class': 'form-control, detail_title', 
+						'style': 'border: 0; padding: 0;',
+						'placeholder': '제목을 입력하세요.',
+						'name' : 'title', 'id' : 'title' }
+			),
+			'price' : forms.TextInput(
+				attrs={ 'class': 'form-control, detail_price',
+						'style': 'width: 70px; border: 0; text-align: right; padding: 0; display: inline-block;',
+						'name' : 'price', 'id' : 'price' }
+			),
+			'wpeople' : forms.TextInput(
+				attrs={ 'class': 'form-control, detail_price',
+						'style': 'width: 35px; border: 0; text-align: right; padding: 0; display: inline-block;',
+						'name' : 'wpeople', 'id' : 'wpeople '}
+			),
+			'location' : forms.TextInput(
+				attrs={ 'class': 'form-control', 'class': 'detail_loca_time',
+						'style': 'width: 100%; border: 0; outline: none; padding: 0;',
+						'placeholder': '장소를 입력하세요. ',
+						'name' : 'place', 'id' : 'place' }
+			),
+			'date' : forms.TextInput(
+				attrs={ 'class': 'form-control', 'class': 'detail_loca_time',
+						'style': 'width: 100%; border: 0; padding: 0;',
+						'placeholder': '날짜를 입력하세요.',
+						'name' : 'date', 'id' : 'date' }
+			),
+			'body' : forms.Textarea (
+				attrs={ 'class': 'form-control, detail_context', 'cols':'50', 'rows':'10',
+						'style': 'width: 100%; border: 0; margin: 0 0 40px 0; padding: 0;',
+						'placeholder': '내용을 입력하세요.' }
 			),
 		}
