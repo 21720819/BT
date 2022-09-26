2aa26487c3ed0404e5956699819016497cef7161
minsol1 <81753858+minsol1@users.noreply.github.com>
2022-09-26 17:35:19 +0900

    검색

--- BT/settings.py
+++ BT/settings.py
@@ -42,7 +42,7 @@ SENDBIRD_API_TOKEN=env("SENDBIRD_API_TOKEN")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
