bd2ba6850e60ff1cf7eab9cc986a19458657ac5e
JuHyun Yoon <21720819@gmail.com>
2022-10-17 00:56:36 +0900

    비율수정_1

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,3 +1,4 @@
+from tkinter import Widget
 from django import forms
 from .models import Review, ReportUser
 
@@ -10,4 +11,11 @@ class UserReportform(forms.ModelForm):
 	class Meta:
 		model = ReportUser
 		fields = ['content']
- 
+  
+		widgets = {
+			'content' : forms.Textarea(
+				attrs={ 'class': 'form-control, detail_context', 'cols':'50', 'rows':'10',
+						'style': 'width: 100%; resize: none; border: 0; margin: 0; padding: 0;',
+						'placeholder': '내용을 입력하세요.' }
+			)
+		}
